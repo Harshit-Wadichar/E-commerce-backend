2668e129ca30572aed758850e9612a7cc5cbef32
Harshit-Wadichar <manohar.wadichar9545@gmail.com>
2026-01-18 23:34:54 +0530

    created basic backend server

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const port = 4000;
+const app = express();
+app.get('/', (req, res) => {
+    res.send("harshit wadichar");
+});
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
+//# sourceMappingURL=app.js.map
